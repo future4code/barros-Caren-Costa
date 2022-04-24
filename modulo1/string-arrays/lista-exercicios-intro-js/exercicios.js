e551e7f81b9e0b53aa30e6c79f277841f8b7b1e9
Caren <carenfcosta@gmail.com>
2022-04-24 11:04:36 -0300

    projeto de fixação

--- modulo1/string-arrays/lista-exercicios-intro-js/exercicios.js
+++ modulo1/string-arrays/lista-exercicios-intro-js/exercicios.js
@@ -107,30 +107,67 @@ function retornaUltimoElemento(array) {
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
+  let ultNum=array.lenght-1
+  let novoArr=array.splice(0,1,array[ultNum])
+  array.splice(ultNum,1,novoArr[0])
+  return array
+
   // implemente sua lógica aqui
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  console.log(string1.toUpperCase()===string2.toUpperCase());
+  console.log(string1.toLowerCase()===string2.toLowerCase());
+  
   // implemente sua lógica aqui
 
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  
+  
+  prompt=('Qual o ano atual?')
+  prompt=("Qual ano você nasceu?")
+  prompt=("Informe o ano da emissão do seu RG:")
+
+  if idade<20 = ("deve ser renovada")
+
+
+// Escreva uma função que pergunta ao usuário o ano atual, o ano de nascimento de uma pessoa, e o ano em que sua carteira de identidade foi emitida (nessa ordem). A função deve imprimir no console um booleano (true ou false) que indica se a carteira precisa ser renovada ou não. A carteira precisa ser renovada segundo os seguintes critérios:
+
+//- Para pessoas com menos de 20 anos, ou exatamente 20 anos, deve ser renovada de 5 em 5 anos (se for exatamente 5 anos, deve ser renovada).
+//- Para pessoas entre 20 e 50 anos, ou exatamente 50, deve ser renovada de 10 em 10 anos (se for exatamente 10 anos, deve ser renovada).
+//- Para pessoas acima dos 50 anos, deve ser renovada de 15 em 15 anos
+//- Dica
+    
+//    Você deve usar comparadores e operadores booleanos para avaliar as condições. Para te ajudar a organizar a lógica, tente criar 3 variáveis separadas, uma para cada condição, e depois compará-las.
 
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
+  if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0));
+  
+  // Escreva uma função que recebe um ano e retorna um booleano (true ou false) que indica se o ano é bissexto. Um ano é bissexto de acordo com as seguintes condições:
+
+//São bissextos** todos os anos múltiplos de 400**.**
+//São bissextos** todos os múltiplos de 4, exceto se for múltiplo de 100 mas não de 400**.**
+//Não são bissextos** todos os demais anos.
 
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+
+// Escreva uma função que faz as seguintes perguntas ao usuário (condições para ser uma pessoa estudante da Labenu):
+
+// Você tem mais de 18 anos?
+//Você possui ensino médio completo?
+//Você possui disponibilidade exclusiva durante os horários do curso?
+//O usuário deve responder essas perguntas com `"sim"` ou `"nao"`.
+//A função deve imprimir no console um booleano (true ou false) que indica se a inscrição para o curso é válida, ou seja, se o usuário pode ou não fazer o curso da Labenu. A inscrição é válida quando todas as respostas para todas as perguntas for positiva.
 
 }
